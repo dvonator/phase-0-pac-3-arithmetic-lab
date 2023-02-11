c9c35d12b6314872270a4952fafd74dd20126333
David Vo <Davidvo1992@gmail.com>
2023-02-11 16:15:13 -0600

    commit tree a4d33a8b5092dde7a377995d5f4ef719618e53c5

---
+++ index.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+function increment(n) {
+        return ++n;  //you want it to add to the n value first before returning the value n
+}
+
+function decrement(n) {
+        return --n;
+}
+
+function makeInt(n) {
+    return parseInt(n,10);
+}
+
+function preserveDecimal(n) {
+    return parseFloat(n,10);
+    
+}
